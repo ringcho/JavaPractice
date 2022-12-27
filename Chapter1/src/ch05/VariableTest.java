86646d675fe86d5bd9da822429f636245a77e035
Min Chul Shin <marsm4rs@gmail.com>
2022-12-28 00:36:59 +0900

    datatype

---
+++ Chapter1/src/ch05/VariableTest.java
@@ -0,0 +1,13 @@
+package ch05;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        byte benumb = -127;
+        int age, count;
+        age = 10;
+        long level = 1234567890000L;
+        System.out.println(age);
+        System.out.println(level);
+        System.out.println(benumb);
+    }
+}
---
+++ Chapter1/src/ch10/ConstantTest.java
@@ -0,0 +1,11 @@
+package ch10;
+
+public class ConstantTest {
+    public static void main(String[] args) {
+        final int MAX_NUM = 200;
+        final int MIN_NUM = 1;
+
+        System.out.println(MAX_NUM);
+        System.out.println(MIN_NUM);
+    }
+}
